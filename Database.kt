f25fa4b8fbf4a36feb804ba0da1eb265c9d4fb99
ST10393028 <131148958+ST10393028@users.noreply.github.com>
2025-05-13 16:24:14 +0200

    Delete Database.kt

--- Database.kt
+++
@@ -1,9 +0,0 @@
-package com.example.budgettrackerapp
-
-import kotlin.reflect.KClass
-
-annotation class Database(
-    val entities: Array<KClass<Expenses>>,
-    val version: Int,
-    val exportSchema: Boolean
-)
